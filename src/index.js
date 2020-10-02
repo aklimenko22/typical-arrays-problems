150c41a11b91e8ba009bec35c4cf01454e514ad5
aklimenko22 <nst22@yandex.ru>
2020-10-02 15:34:20 +0300

    feat: add solution for min

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+  let min = array[0];
+  for (let i = 0; i < array.length; i++) {
+      if (min > array[i]) min = array[i];
+  }
+  return min;
+};
+console.log (min);
 
 exports.max = function max (array) {
   return 0;
