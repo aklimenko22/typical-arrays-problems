2fc6e6128d7df2400f46277a1a04b95b8e0bdc24
aklimenko22 <nst22@yandex.ru>
2020-10-03 12:17:47 +0300

    feat: add solution for max

--- src/index.js
+++ src/index.js
@@ -3,14 +3,17 @@ exports.min = function min (array) {
   let min = array[0];
   for (let i = 0; i < array.length; i++) {
       if (min > array[i]) min = array[i];
-  }
-  return min;
+      } 
+        return min;
 };
-console.log (min);
 
 exports.max = function max (array) {
-  return 0;
-}
+  var max = array[0];
+  for (var i = 0; i < array.length; i++) { 
+      if (max < array[i]) max = array[i]; 
+  }
+  return max;
+};
 
 exports.avg = function avg (array) {
   return 0;
