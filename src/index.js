7420d9a97a96d27f1699ea174a32f48a120b6a75
aklimenko22 <nst22@yandex.ru>
2020-10-03 12:31:52 +0300

    feat: add solution for average

--- src/index.js
+++ src/index.js
@@ -16,5 +16,10 @@ exports.max = function max (array) {
 };
 
 exports.avg = function avg (array) {
-  return 0;
+  var total = 0;
+  for(var i = 0; i < array.length; i++) {
+      total += array[i];
+  }
+  var avg = total / array.length;
+  return avg;
 }
